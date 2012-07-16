871378ab76864b2e79e489a74d59449e3008fe51
DeShawn Williams <deswilliams@zappos.com>
2012-07-15 20:53:46 -0700

    readded config for unbuilt version

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
+var require = require.config({
+  // most module definitions are here
+  baseUrl: "./modules/",
+  paths: {
+    flavors: '/src/flavors',
+    settings: '/src/settings',
+    base: '/src/modules/base',
+    component: '/src/modules/component',
+    region: '/src/modules/region',
+    tab: '/src/modules/tab',
+    view: '/src/modules/view',
+    vegas: '/src/vegas',
+    underscore: '/src/libraries/underscore/underscore'
+  }
+});
+
 require(['vegas'], function (vegas) {
   // once loaded, say hello
   console.log('i went to vegas and all i got was this lousy text editor.');
