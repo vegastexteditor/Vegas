bfd44d75e52ad066d0c052b43f0c199104387811
DeShawn Williams <deswilliams@zappos.com>
2012-07-15 23:23:31 -0700

    changed up build structure and moved r.js

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ./build/build_dependencies.sh
-node r.js -o baseUrl=src/modules paths.main=../main paths.vegas=../vegas paths.underscore=../libraries/underscore/underscore name=main out=main-built.js
+node build/r.js -o baseUrl=src/modules paths.main=../main paths.vegas=../vegas paths.underscore=../libraries/underscore/underscore name=main out=built/main-built.js
